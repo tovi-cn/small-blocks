ed8095f020496de870f6bf027fc6081b38f55760
Carl Enlund <carl.enlund@gmail.com>
2020-01-01 23:48:11 +0100

    Set up 3D controls

--- src/game.cc
+++ src/game.cc
@@ -2,27 +2,28 @@
 
 #include <iostream>
 
+#include "glm/gtx/euler_angles.hpp"
 #include "glm/gtx/transform.hpp"
 #include "glm/vec3.hpp"
 #include "glm/mat4x4.hpp"
 
 static const struct {
-  float x, y;
+  float x, y, z;
   float r, g, b;
 } kVertices[3] = {
-  { -0.6f, -0.4f, 1.f, 0.f, 0.f },
-  {  0.6f, -0.4f, 0.f, 1.f, 0.f },
-  {   0.f,  0.6f, 0.f, 0.f, 1.f }
+  { -0.6f, -0.4f, 0.0f, 1.f, 0.f, 0.f },
+  {  0.6f, -0.4f, 0.0f, 0.f, 1.f, 0.f },
+  {   0.f,  0.6f, 0.0f, 0.f, 0.f, 1.f }
 };
 
 static const char *kVertexShaderText =
 "#version 330 core\n"
 "uniform mat4 MVP;\n"
-"layout (location = 0) in vec2 vPos;\n"
+"layout (location = 0) in vec3 vPos;\n"
 "layout (location = 1) in vec3 vCol;\n"
 "out vec3 color;\n"
 "void main() {\n"
-"  gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+"  gl_Position = MVP * vec4(vPos, 1.0);\n"
 "  color = vCol;\n"
 "}\n";
 
@@ -34,20 +35,45 @@ static const char *kFragmentShaderText =
 "  gl_FragColor = vec4(color, 1.0);\n"
 "}\n";
 
-Game::Game() {
-  // Initialize Window with OpenGL
+Game::Game() 
+    : window_(nullptr),
+      pressed_keys_(),
+      camera_position_(0.0f), camera_rotation_(0.0f),
+      mouse_last_x_(0.0), mouse_last_y_(0.0) {
+  camera_rotation_.y = glm::radians(180.0f);
+}
+
+Game::~Game() {
+  glfwDestroyWindow(window_);
+  glfwTerminate();
+}
+
+bool Game::Initialize() {
+  // Initialize Window
+
   glfwSetErrorCallback(OnGlfwError);
   if (!glfwInit()) {
-    exit(1);
+    return false;
   }
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  window_ = glfwCreateWindow(800, 480, "Recurse Builder", NULL, NULL);
+  window_ = glfwCreateWindow(800, 600, "Recurse Builder", NULL, NULL);
   if (!window_) {
     glfwTerminate();
-    exit(1);
+    return false;
   }
-  glfwSetKeyCallback(window_, OnKeyPress);
+  glfwSetWindowUserPointer(window_, this);
+
+  glfwSetInputMode(window_, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+  if (glfwRawMouseMotionSupported()) {
+    glfwSetInputMode(window_, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
+  }
+
+  glfwSetMouseButtonCallback(window_, OnMouseButtonEvent);
+  glfwSetKeyCallback(window_, OnKeyEvent);
+
+  // Initialize OpenGL
+
   glfwMakeContextCurrent(window_);
   gladLoadGL();
   glfwSwapInterval(1);
@@ -66,12 +92,12 @@ Game::Game() {
 
   // Position
   glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE,
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
                         sizeof(kVertices[0]), (void *)0);
   // Color
   glEnableVertexAttribArray(1);
   glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE,
-                        sizeof(kVertices[0]), (void *)(sizeof(float) * 2));
+                        sizeof(kVertices[0]), (void *)(3 * sizeof(float)));
 
   glBindVertexArray(0);
 
@@ -91,32 +117,96 @@ Game::Game() {
   glLinkProgram(program_);
 
   mvp_location_ = glGetUniformLocation(program_, "MVP");
-}
 
-Game::~Game() {
-  glfwDestroyWindow(window_);
-  glfwTerminate();
-}
-
-bool Game::Initialize() {
   return true;
 }
 
 void Game::Run() {
+  glfwGetCursorPos(window_, &mouse_last_x_, &mouse_last_y_);
+  double last_time = glfwGetTime();
+
   while (!glfwWindowShouldClose(window_)) {
+    double current_time = glfwGetTime();
+    float delta_time = current_time - last_time;
+    last_time = current_time;
+
+    // Handle mouse and keyboard input
+
+    double mouse_x;
+    double mouse_y;
+    glfwGetCursorPos(window_, &mouse_x, &mouse_y);
+    float mouse_delta_x = mouse_x - mouse_last_x_;
+    float mouse_delta_y = mouse_y - mouse_last_y_;
+    mouse_last_x_ = mouse_x;
+    mouse_last_y_ = mouse_y;
+
+    float mouse_sensitivity = 0.007f;
+    camera_rotation_.x += -mouse_delta_y * mouse_sensitivity;
+    camera_rotation_.y += -mouse_delta_x * mouse_sensitivity;
+    camera_rotation_.x = glm::clamp(camera_rotation_.x, glm::radians(-90.0f),
+                                    glm::radians(90.0f));
+
+    float speed = 1.0f;
+    glm::mat4 rotation(1.0f);
+    rotation = glm::rotate(camera_rotation_.y, glm::vec3(0.0f, 1.0f, 0.0f)) * rotation;
+    glm::vec3 forward = glm::vec3(rotation * glm::vec4(0.0f, 0.0f, -1.0f, 1.0f));
+    glm::vec3 up(0.0f, 1.0f, 0.0f);
+    glm::vec3 right = glm::normalize(glm::cross(forward, up));
+
+    glm::vec3 direction(0.0f);
+    if (pressed_keys_.test(GLFW_KEY_W)) {
+      direction += forward;
+    }
+    if (pressed_keys_.test(GLFW_KEY_S)) {
+      direction -= forward;
+    }
+    if (pressed_keys_.test(GLFW_KEY_A)) {
+      direction -= right;
+    }
+    if (pressed_keys_.test(GLFW_KEY_D)) {
+      direction += right;
+    }
+    if (direction != glm::vec3(0.0f)) {
+      camera_position_ += glm::normalize(direction) * speed * delta_time;
+    }
+
+    if (pressed_keys_.test(GLFW_KEY_SPACE)) {
+      camera_position_ += up * speed * delta_time;
+    }
+    if (pressed_keys_.test(GLFW_KEY_LEFT_CONTROL)) {
+      camera_position_ -= up * speed * delta_time;
+    }
+
+    // Render scene
+
     int width;
     int height;
     glfwGetFramebufferSize(window_, &width, &height);
     float aspect = (float)width / height;
     glViewport(0, 0, width, height);
-    glClear(GL_COLOR_BUFFER_BIT);
 
-    glm::mat4 m = glm::mat4(1.0f);
-    m = glm::rotate((float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
-    glm::mat4 p = glm::ortho(-aspect, aspect, -1.0f, 1.0f, 1.0f, -1.0f);
-    glm::mat4 mvp = p * m;
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+    glm::mat4 m(1.0f);
+    m = glm::rotate((float)glfwGetTime(), glm::vec3(0.0f, 1.0f, 0.0f)) * m;
+    m = glm::translate(glm::vec3(0.0f, 0.0f, 1.0f)) * m;
+
+    glm::mat4 v(1.0f);
+    v = glm::translate(-camera_position_) * v;
+    v = glm::rotate(-camera_rotation_.y, glm::vec3(0.0f, 1.0f, 0.0f)) * v;
+    v = glm::rotate(-camera_rotation_.x, glm::vec3(1.0f, 0.0f, 0.0f)) * v;
+
+    float fov = 80.0f;
+    float near = 0.1f;;
+    float far = 100.0f;
+    glm::mat4 p = glm::perspective(glm::radians(fov), aspect, near, far);
+
+    glm::mat4 mvp = p * v * m;
+
     glUseProgram(program_);
     glUniformMatrix4fv(mvp_location_, 1, GL_FALSE, (const GLfloat *)&mvp[0][0]);
+
 
     glBindVertexArray(vertex_array_);
     glDrawArrays(GL_TRIANGLES, 0, 3);
@@ -126,6 +216,53 @@ void Game::Run() {
 
     glfwSwapBuffers(window_);
     glfwPollEvents();
+  }
+}
+
+void Game::MouseDown(int button) {
+  pressed_mouse_buttons_.set(button);
+}
+
+void Game::MouseUp(int button) {
+  pressed_mouse_buttons_.reset(button);
+}
+
+void Game::KeyDown(int key) {
+  if (key == GLFW_KEY_W) {
+  }
+  pressed_keys_.set(key);
+
+  if (key == GLFW_KEY_ESCAPE) {
+    glfwSetWindowShouldClose(window_, GLFW_TRUE);
+  }
+}
+
+void Game::KeyUp(int key) {
+  pressed_keys_.reset(key);
+}
+
+void Game::OnMouseButtonEvent(GLFWwindow *window, int button,
+                              int action, int mods) {
+  (void)mods;
+
+  Game *game = static_cast<Game *>(glfwGetWindowUserPointer(window));
+  if (action == GLFW_PRESS) {
+    game->MouseDown(button);
+  } else if (action == GLFW_RELEASE) {
+    game->MouseUp(button);
+  }
+}
+
+void Game::OnKeyEvent(GLFWwindow *window, int key, int scancode,
+                      int action, int mods) {
+  (void)scancode;
+  (void)mods;
+
+  Game *game = static_cast<Game *>(glfwGetWindowUserPointer(window));
+  if (action == GLFW_PRESS || action == GLFW_REPEAT) {
+    game->KeyDown(key);
+  } else if (action == GLFW_RELEASE) {
+    game->KeyUp(key);
   }
 }
 
@@ -149,13 +286,4 @@ void GLAPIENTRY Game::OnGlError(GLenum source,
             << ", type = 0x" << type
             << ", severity = 0x" << severity
             << ", message = " << message << "\n";
-}
-
-void Game::OnKeyPress(GLFWwindow *window, int key, int scancode,
-                      int action, int mods) {
-  (void)scancode;
-  (void)mods;
-  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-    glfwSetWindowShouldClose(window, GLFW_TRUE);
-  }
 }
--- src/game.h
+++ src/game.h
@@ -1,9 +1,12 @@
 #ifndef GAME_H_
 #define GAME_H_
 
+#include <bitset>
+
 #include "glad/glad.h"
 #define GLFW_INCLUDE_NONE
 #include "GLFW/glfw3.h"
+#include "glm/vec3.hpp"
 
 class Game {
  public:
@@ -13,7 +16,16 @@ class Game {
   bool Initialize();
   void Run();
 
+  void MouseDown(int button);
+  void MouseUp(int button);
+  void KeyDown(int key);
+  void KeyUp(int key);
+
  private:
+  static void OnMouseButtonEvent(GLFWwindow *window, int button,
+                                 int action, int mods);
+  static void OnKeyEvent(GLFWwindow *window, int key, int scancode,
+                         int action, int mods);
   static void OnGlfwError(int error, const char* description);
   static void GLAPIENTRY OnGlError(GLenum source,
                                    GLenum type,
@@ -22,15 +34,20 @@ class Game {
                                    GLsizei length,
                                    const GLchar *message,
                                    const void *user_param);
-  static void OnKeyPress(GLFWwindow *window, int key, int scancode,
-                         int action, int mods);
 
   GLFWwindow *window_;
+  std::bitset<32> pressed_mouse_buttons_;
+  std::bitset<1024> pressed_keys_;
 
   GLuint vertex_array_;
   GLuint vertex_buffer_;
   GLuint program_;
   GLuint mvp_location_;
+
+  glm::vec3 camera_position_;
+  glm::vec3 camera_rotation_;
+  double mouse_last_x_;
+  double mouse_last_y_;
 };
 
 #endif  // GAME_H_
